b5cb674d3396c96b322c1385b267d6db26dee995
Egba Frank <Egbafrank72@gmail.com>
2024-12-07 23:32:14 +0000

    create express server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
