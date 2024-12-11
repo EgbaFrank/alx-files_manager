f814fd0e8d49d6ca9bb926b91667b3a5db8d3e45
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-12-11 16:06:43 +0100

    added github actions workflow for CI

---
+++ tests/redisClient.test.js
@@ -0,0 +1,29 @@
+import { expect, use, should } from 'chai';
+import chaiHttp from 'chai-http';
+import { promisify } from 'util';
+import redisClient from '../utils/redis';
+
+use(chaiHttp);
+should();
+
+describe('redis Client', () => {
+  before(async () => {
+    await redisClient.client.flushall('ASYNC');
+  });
+
+  after(async () => {
+    await redisClient.client.flushall('ASYNC');
+  });
+
+  it('shows that connection is alive', async () => {
+    expect(redisClient.isAlive()).to.equal(true);
+  });
+
+  it('returns key as null because it does not exist', async () => {
+    expect(await redisClient.get('myKey')).to.equal(null);
+  });
+
+  it('set key can be called without issue', async () => {
+    expect(await redisClient.set('myKey', 12, 1)).to.equal(undefined);
+  });
+});
